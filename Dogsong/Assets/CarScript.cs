456799b4431a527adf7a02465a9b045924bdee51
kaiyihuang <iheartgrues@gmail.com>
2017-01-15 07:09:28 -0700

    kek

---
+++ Dogsong/Assets/CarScript.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour {
+	public bool moving = false;
+	void Start()
+	{
+		
+		StartCoroutine (fuckthis());
+		//MoveObject(new Vector3 (1160.12f, 601.1f, -351.055f), new Vector3 (525f, 601f, 258f), 12.0f);
+		//RotateLeft (10f);
+		//StartCoroutine(MoveFromTo (new Vector3 (525f, 601f, 258f),new Vector3 (385f, 601f, -5829f), 60.0f));
+		//StartCoroutine(RotateRight(9f));
+		//MoveFromTo (new Vector3 (385f, 601f, -5829f),new Vector3 (-1257f, 601f, -5829f), 15.0f);
+		//RotateRight(9f);
+		//MoveFromTo (new Vector3 (-1257f, 601f, -5829f),new Vector3 (-1257f, 601f, -4424f), 25.0f);
+	}
+	void Update()
+	{
+	}
+	void MoveObject(Vector3 pointA, Vector3 pointB, float time ){
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.position = Vector3.Lerp(pointA, pointB, t); // set position proportional to t
+			}
+			moving = false; // finished moving
+
+
+	}
+	IEnumerator fuckthis()
+	{
+		
+		float time = 10.0f;
+		if (!moving){ // do nothing if already moving
+			moving = true; // signals "I'm moving, don't bother me!"
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.position = Vector3.Lerp(new Vector3 (1160.12f, 601.1f, -351.055f), new Vector3 (525f, 601f, 258f), t); // set position proportional to t
+				yield return 0;
+			}
+			moving = false; // finished moving
+
+		}
+		transform.position = new Vector3 (525f, 601f, 258f);
+		time = 4.0f;
+		if (!moving){ // do nothing if already moving
+			moving = true; // signals "I'm moving, don't bother me!"
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.Rotate (-Vector3.up * Time.deltaTime * 30);
+				yield return 0;
+			}
+			moving = false; // finished moving
+
+		}
+		time = 60.0f;
+		if (!moving){ // do nothing if already moving
+			moving = true; // signals "I'm moving, don't bother me!"
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.position = Vector3.Lerp(new Vector3 (525f, 601f, 258f),new Vector3 (385f, 601f, -5829f), t); // set position proportional to t
+				yield return 0;
+			}
+			moving = false; // finished moving
+
+		}
+		transform.position = new Vector3 (385f, 601f, -5829f);
+		time = 3.0f;
+		if (!moving){ // do nothing if already moving
+			moving = true; // signals "I'm moving, don't bother me!"
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.Rotate (Vector3.up * Time.deltaTime * 30);
+				yield return 0;
+			}
+			moving = false; // finished moving
+
+		}
+		time = 15.0f;
+		if (!moving){ // do nothing if already moving
+			moving = true; // signals "I'm moving, don't bother me!"
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.position = Vector3.Lerp(new Vector3 (385f, 601f, -5829f),new Vector3 (-1257f, 601f, -5829f), t); // set position proportional to t
+				yield return 0;
+			}
+			moving = false; // finished moving
+
+		}
+		transform.position = new Vector3 (-1257f, 601f, -5829f);
+		time = 3.0f;
+		if (!moving){ // do nothing if already moving
+			moving = true; // signals "I'm moving, don't bother me!"
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.Rotate (Vector3.up * Time.deltaTime * 30);
+				yield return 0;
+			}
+			moving = false; // finished moving
+
+		}
+		time = 17.0f;
+		if (!moving){ // do nothing if already moving
+			moving = true; // signals "I'm moving, don't bother me!"
+			float t = 0f;
+			while (t < 1f){
+				t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+				transform.position = Vector3.Lerp(new Vector3 (-1257f, 601f, -5829f),new Vector3 (-1257f, 601f, -4424f), t); // set position proportional to t
+				yield return 0;
+			}
+			moving = false; // finished moving
+
+		}
+		transform.position = new Vector3 (-1257f, 601f, -4424f);
+		yield return new WaitForSeconds (3.0f);
+		StartCoroutine(transition ());
+	}
+	public Transform doggo;
+	public Transform doggo2;
+	public Transform SecondCam;
+
+	IEnumerator transition()
+	{
+		
+		GameObject d = transform.FindChild ("car_exterior").gameObject;
+		GameObject g = d.transform.FindChild ("R_car_door").gameObject;
+		var k = g.GetComponent<Animation>();
+		k.Play ();
+		yield return new WaitForSeconds (1.8f);
+		doggo.parent = null;
+		GameObject meep = doggo.FindChild ("Main Camera").gameObject;
+		ScreenFader.fadeIn = false;
+		yield return new WaitForSeconds (16.5f);
+		doggo.gameObject.SetActive(false);
+		doggo2.gameObject.SetActive (true);
+		meep.GetComponent<Camera> ().enabled = false;
+		meep.GetComponent<AudioListener> ().enabled = false;
+		SecondCam.GetComponent<Camera> ().enabled = true;
+		SecondCam.GetComponent<AudioListener> ().enabled = true;
+		ScreenFader.fadeIn = true;
+		k.Rewind ();
+
+
+		float time = 7f;
+		float t = 0f;
+		while (t < 1f){
+			t += Time.deltaTime / time; // sweeps from 0 to 1 in time seconds
+			transform.position = Vector3.Lerp(new Vector3 (-1257f, 601f, -4424f),new Vector3 (-1257f, 601f, -2939f), t); // set position proportional to t
+			yield return 0;
+		}
+
+		yield return new WaitForSeconds (14.0f);
+		ScreenFader.fadeIn = false;
+
+	}
+
+	}
+
+
+
---
+++ Dogsong/Assets/Scripts/WindowScript.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace VRStandardAssets.Utils
+{
+	// This class works similarly to the SelectionRadial class except
+	// it has a physical manifestation in the scene.  This can be
+	// either a UI slider or a mesh with the SlidingUV shader.  The
+	// functions as a bar that fills up whilst the user looks at it
+	// and holds down the Fire1 button.
+	public class WindowScript : MonoBehaviour
+	{
+		public event Action OnBarFilled;                                    // This event is triggered when the bar finishes filling.
+
+
+		[SerializeField] private float m_Duration = 2f;                     // The length of time it takes for the bar to fill.
+		[SerializeField] private VRInteractiveItem m_InteractiveItem;       // Reference to the VRInteractiveItem to determine when to fill the bar.
+		[SerializeField] private VRInput m_VRInput;                         // Reference to the VRInput to detect button presses.
+
+
+		private bool m_BarFilled;                                           // Whether the bar is currently filled.
+		private bool m_GazeOver;                                            // Whether the user is currently looking at the bar.
+		private float m_Timer;                                              // Used to determine how much of the bar should be filled.
+		private Coroutine m_FillBarRoutine;                                 // Reference to the coroutine that controls the bar filling up, used to stop it if required.
+
+
+		private const string k_SliderMaterialPropertyName = "_SliderValue"; // The name of the property on the SlidingUV shader that needs to be changed in order for it to fill.
+
+
+		private void OnEnable ()
+		{
+
+			m_InteractiveItem.OnOver += HandleOver;
+		}
+			
+
+		private void Update ()
+		{
+
+		}
+
+
+		private void HandleOver ()
+		{
+			// The user is now looking at the bar.
+			m_GazeOver = true;
+			Debug.Log ("IsOver");
+			Renderer Rend = GetComponent<Renderer> ();
+			Rend.material.shader = Shader.Find ("Specular");
+				Rend.material.SetColor("_SpecColor",Color.red);	
+
+		}
+	}
+}
